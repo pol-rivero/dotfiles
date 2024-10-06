fe3155c07944ae6eb30bafdeb8a27d37f1f7dd01
Pol Rivero <65060696+p-rivero@users.noreply.github.com>
2024-10-06 20:05:17 +0200

    Copy swaync theme from hyprnova

--- dot-config/hypr/scripts/volume_control.sh
+++ dot-config/hypr/scripts/volume_control.sh
@@ -3,7 +3,7 @@
 # Scripts for volume controls for audio and mic 
 
 iDIR="$HOME/.config/swaync/icons"
-sDIR="."
+sDIR=$(dirname "$0")
 
 STEP=2
 
@@ -37,7 +37,7 @@ notify_user() {
         notify-send -e -h string:x-canonical-private-synchronous:volume_notif -u low -i "$(get_icon)" "Volume: Muted"
     else
         notify-send -e -h int:value:"$(get_volume | sed 's/%//')" -h string:x-canonical-private-synchronous:volume_notif -u low -i "$(get_icon)" "Volume: $(get_volume)"
-        "$sDIR/Sounds.sh" --volume
+        "$sDIR/play-system-sound.sh" --volume
     fi
 }
 
