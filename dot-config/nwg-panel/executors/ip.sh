a2c3778ae2ca2e370345af7f00a3d0ca256b4585
privero <65060696+p-rivero@users.noreply.github.com>
2024-11-13 08:38:08 +0100

    Update ip.sh

--- dot-config/nwg-panel/executors/ip.sh
+++ dot-config/nwg-panel/executors/ip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 get_ip() {
-	prefix=$1
-	shift
+    prefix=$1
+    shift
     for iface in "$@"; do
         # Check if the interface is up and has an IP address
         if ip link show "$iface" | grep -q "state UP"; then
@@ -15,21 +15,19 @@ get_ip() {
     done
 }
 
-vpn_interfaces=$(ip -o link show | awk -F': ' '{print $2}' | grep -E '^(ppp|tun|tap|wg)')
-
-ethernet_interfaces=$(ip -o link show | awk -F': ' '{print $2}' | grep -E '^(eth|enx|enp)')
-
-wifi_interfaces=$(ip -o link show | awk -F': ' '{print $2}' | grep -E '^(wlan|wlo)')
-
+interface_names=$(ip -o link show | awk -F': ' '{print $2}')
 # Prioritize and get the IP address in order of VPN > Ethernet > WiFi
+vpn_interfaces=$(echo "$interface_names" | grep -E '^(tun|tap|ipsec|ppp|vtun|openvpn|wg|wireguard)')
 if [ -n "$vpn_interfaces" ]; then
     get_ip  $vpn_interfaces
 fi
 
+ethernet_interfaces=$(echo "$interface_names" | grep -E '^(eth|enx|enp)')
 if [ -n "$ethernet_interfaces" ]; then
     get_ip  $ethernet_interfaces
 fi
 
+wifi_interfaces=$(echo "$interface_names" | grep -E '^(wlan|wlo)')
 if [ -n "$wifi_interfaces" ]; then
     get_ip  $wifi_interfaces
 fi
